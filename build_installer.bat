ae27472550a50a6b1d9c4235bd32c09daa6536bd
Daniel DT (OBS) <daniel.dt.inidep@gmail.com>
2026-06-10 11:56:40 -0300

    fix(build): eliminar caracter BOM de build_installer.bat

--- build_installer.bat
+++ build_installer.bat
@@ -1,4 +1,4 @@
-﻿@echo off
+@echo off
 echo ======================================================
 echo Generando Instalador para Control de mareas
 echo ======================================================
